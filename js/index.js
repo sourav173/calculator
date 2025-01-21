1c52eb595641276afe6b80cbc8accaaaed744339
sourav173 <sourav.sardar@myyahoo.com>
2025-01-21 20:30:47 +0530

    function created for number

--- js/index.js
+++ js/index.js
@@ -2,5 +2,8 @@ let display = document.querySelector("#display");
 // console.log(display);
 let currentInput = "";
 
-
+function appendNumber(number) {
+    currentInput += number;
+    display.value = currentInput;
+}
 
